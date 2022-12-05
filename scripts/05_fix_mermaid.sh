cd777b9a6da109889217ea4c7376eaa9ec553d3b
Edward Ross <edward@skeptric.com>
2022-12-05 23:14:10 +1100

    In fix_mermaid use OUT_PATH everywhere

--- scripts/05_fix_mermaid.sh
+++ scripts/05_fix_mermaid.sh
@@ -5,4 +5,4 @@ source common.sh
 find "${OUT_PATH}" -name 'index.md' -exec sed -i -e 's|^{{ *<mermaid> *}}|```{mermaid}|' -e 's|^{{< */mermaid *>}}|```|' {} \;
 
 # They are now executable: rename to .qmd
-find output -name 'index.md' | xargs -n1 grep -l '```{mermaid}' | xargs -n1 sh -c 'mv "$0" "${0%.md}.qmd"'
+find "${OUT_PATH}" -name 'index.md' | xargs -n1 grep -l '```{mermaid}' | xargs -n1 sh -c 'mv "$0" "${0%.md}.qmd"'
